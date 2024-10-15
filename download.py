473999b2b12b1f738b94638fbcef8a0af89f5459
Karis <td.kha.18@gmail.com>
2024-10-15 21:24:10 +0700

    Update download.py

--- download.py
+++ download.py
@@ -4,14 +4,21 @@ import os
 ASEPRITE_REPOSITORY = 'aseprite/aseprite'
 SKIA_REPOSITORY = 'aseprite/skia'
 SKIA_RELEASE_FILE_NAME = 'Skia-Windows-Release-x64.zip'
+VERSION_FILE = 'version.txt'
 
 def get_latest_tag_aseprite():
 	response = requests.get(f'https://api.github.com/repos/{ASEPRITE_REPOSITORY}/releases/latest')
 	response_json = response.json()
 	return response_json['tag_name']
 
+def get_current_version():
+	if os.path.exists(VERSION_FILE):
+		with open(VERSION_FILE, 'r') as f:
+			return f.read().strip()
+	return None
+
 def save_aseprite_tag(tag):
-	with open('version.txt', 'w') as f:
+	with open(VERSION_FILE, 'w') as f:
 		f.write(tag)
 
 def clone_aseprite(tag):
@@ -38,8 +45,13 @@ def download_skia_for_windows(tag):
 
 if __name__ == '__main__':
 	aseprite_tag = get_latest_tag_aseprite()
-	clone_aseprite(aseprite_tag)
-	save_aseprite_tag(aseprite_tag)
+	current_version = get_current_version()
 
-	skia_tag = get_latest_tag_skia()
-	download_skia_for_windows(skia_tag)
+	if aseprite_tag != current_version:
+		clone_aseprite(aseprite_tag)
+		save_aseprite_tag(aseprite_tag)
+
+		skia_tag = get_latest_tag_skia()
+		download_skia_for_windows(skia_tag)
+	else:
+		print("Already at the latest version.")
